41a6b8afd6dfdc75db2de09bec6a6e9628a7cd97
Jose Velasco <jose.juarez.velasco@gmail.com>
2020-01-01 19:17:37 -0800

    cleaned up subscription

--- src/app/flashcard/flashcard-edit-deck-menu/flashcard-edit-deck-menu.component.ts
+++ src/app/flashcard/flashcard-edit-deck-menu/flashcard-edit-deck-menu.component.ts
@@ -1,24 +1,26 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter, OnDestroy } from '@angular/core';
 import { PageRoute } from 'nativescript-angular/router';
 import { EventData } from 'tns-core-modules/ui/page/page';
 import { FlashcardService } from '../flashcard.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ns-flashcard-edit-deck-menu',
   templateUrl: './flashcard-edit-deck-menu.component.html',
   styleUrls: ['./flashcard-edit-deck-menu.component.css']
 })
-export class FlashcardEditDeckMenuComponent implements OnInit {
+export class FlashcardEditDeckMenuComponent implements OnInit, OnDestroy {
     TitleOfDeck: string = '';
     dynamicParamSelectedDeckIndex: number;
     onsaveFlashcardDeckEvent: EventEmitter<boolean> = new EventEmitter();
+    pagerouterSub: Subscription;
 
   constructor(
       private pageRoute: PageRoute,
       private flashcardService: FlashcardService) { }
 
   ngOnInit() {
-      this.pageRoute.activatedRoute.subscribe(activatedRoute => {
+    this.pagerouterSub = this.pageRoute.activatedRoute.subscribe(activatedRoute => {
           activatedRoute.paramMap.subscribe(paramMap => {
               this.dynamicParamSelectedDeckIndex = parseInt(paramMap.get('id'));
               this.TitleOfDeck = this.flashcardService.getAFlashcardDeck(this.dynamicParamSelectedDeckIndex).title;
@@ -28,6 +30,10 @@ export class FlashcardEditDeckMenuComponent implements OnInit {
 
   onSaveFlashcardDeck(eventData: EventData) {
       this.onsaveFlashcardDeckEvent.emit(true);
+  }
+
+  ngOnDestroy() {
+    this.pagerouterSub.unsubscribe();
   }
 
 }
